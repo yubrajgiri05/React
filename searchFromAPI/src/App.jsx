ca5be427d629541399f866fd37597ecdfcd0fde2
yubrajgiri05 <giriyubraj787898@gmail.com>
2024-01-14 22:52:42 +0545

    search

---
+++ searchFromAPI/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Home from './Home'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+const App = () => {
+  return (
+    <>
+      <Home/>
+    </>
+  )
+}
+
+export default App
---
+++ searchFromAPI/src/Home.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react'
+
+const Home = () => {
+    const [data, setData] = useState([])
+    const [records, setRecords] = useState(data)
+
+    useEffect(()=>{
+        fetch(`https://jsonplaceholder.typicode.com/users`)
+	.then(response => response.json())
+	.then(data => {
+        setData(data)
+        setRecords(data)
+    })
+	.catch(err => console.error(err));
+
+        // fetch.get(`https://jsonplaceholder.typicode.com/users`)
+        // .then(res=> setData(res.Data))
+        // .catch(err=> console.log(err));
+    },[])
+    const Filter =(event) =>{
+        setRecords(data.filter(f=> f.name.toLowerCase().includes(event.target.value)))
+
+    }
+  return (
+    <>
+      <div className="p-5">
+        <input type="text" className='form-control' onClick={Filter} placeholder='search data' />
+        <table className='bg-success m-5'>
+            <thead>
+                <tr className='bg-danger '>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>company</th>
+                </tr>
+            </thead>
+            <tbody>
+                {records.map((d,i)=>(
+                <tr key={i}>
+                    <td>{d.id}</td>
+                    <td>{d.name}</td>
+                    <td>{d.email}</td>
+                    <td>{d.phone}</td>
+                    <td>{d.company.name}</td>
+                </tr>
+                ))}
+            </tbody>
+        </table>
+      </div>
+    </>
+  )
+}
+
+export default Home
